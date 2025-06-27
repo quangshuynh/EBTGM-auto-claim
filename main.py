f0bdddc9b28b79c85c9220d52f7f038a22359ccd
Quang <qth9368@rit.edu>
2025-06-27 17:30:28 -0400

    more functions

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ OPEN_BUTTON   = [(84, 1027), (85, 1028)]
 CLAIM_BUTTONS = [
     (1452, 788), # 5xp
     (1295, 780), # 10xp 
-    (1104, 791), # 30xp
-    (951,  784), # 100xp
+    #(1104, 791), # 30xp
+    #(951,  784), # 100xp
     #(1053, 151) # save to bank
     ]
 INTERVAL      = 3.0    
@@ -29,18 +29,20 @@ def is_window_up() -> bool:
             return True
     return False
 
-def main():
+def start_txt():
     for i in range(3, 0, -1):
-        print(f"Starting in {i}s…")
+        print(f"  Starting in {i}s…")
         time.sleep(1)
-    print("Auto-claimer running! Ctrl-C to stop")
+    print(" Auto-claimer running! Ctrl-C to stop")
+
+def move_click():
     while True:
-        if not is_window_up():
+        if not is_window_up():  # open level window
             print(f"[{time.strftime('%H:%M:%S')}] level window closed → opening")
             for i, pos in enumerate(OPEN_BUTTON, 1):
                 pd.moveTo(*pos)
                 pd.click()
-        else:
+        else: # claim rewards
             print(f"[{time.strftime('%H:%M:%S')}] level window open → claiming")
             for i, pos in enumerate(CLAIM_BUTTONS, 1):
                 pd.moveTo(*pos) # move to button
@@ -51,6 +53,10 @@ def main():
                 print(f"  • double-clicked #{i} at {pos}")
                 time.sleep(0.5)               
         time.sleep(INTERVAL)
+
+def main():
+    start_txt()
+    move_click()
 
 if __name__ == "__main__":
     main()
