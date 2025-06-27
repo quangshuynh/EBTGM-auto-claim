e498eb4d55cbdd39edb94398e698fcb7f4f03fee
Quang <qth9368@rit.edu>
2025-06-27 17:26:08 -0400

    made more efficient

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ CLOSE_CX, CLOSE_CY = 1690, 414 # center of window close button
 BOX_SIZE = 100 
 REGION = (CLOSE_CX - BOX_SIZE // 2, CLOSE_CY - BOX_SIZE // 2, BOX_SIZE, BOX_SIZE)
 
-OPEN_BUTTON   = (84, 1027)
+OPEN_BUTTON   = [(84, 1027), (85, 1028)]
 CLAIM_BUTTONS = [
     (1452, 788), # 5xp
     (1295, 780), # 10xp 
     (1104, 791), # 30xp
     (951,  784), # 100xp
-    (1053, 151) # save to bank
+    #(1053, 151) # save to bank
     ]
 INTERVAL      = 3.0    
 
@@ -30,18 +30,16 @@ def is_window_up() -> bool:
     return False
 
 def main():
-    print("Starting in 3s…")
-    time.sleep(1)
-    print("Starting in 2s…")
-    time.sleep(1)
-    print("Starting in 1s…")
-    time.sleep(1)
+    for i in range(3, 0, -1):
+        print(f"Starting in {i}s…")
+        time.sleep(1)
     print("Auto-claimer running! Ctrl-C to stop")
     while True:
         if not is_window_up():
             print(f"[{time.strftime('%H:%M:%S')}] level window closed → opening")
-            pd.moveTo(*OPEN_BUTTON)
-            pd.click()
+            for i, pos in enumerate(OPEN_BUTTON, 1):
+                pd.moveTo(*pos)
+                pd.click()
         else:
             print(f"[{time.strftime('%H:%M:%S')}] level window open → claiming")
             for i, pos in enumerate(CLAIM_BUTTONS, 1):
